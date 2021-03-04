169629f49035dbbb58fd7dad68dd27d1d2d86bc2
Alex Kotz <alexKotz@github.com>
2021-03-04 00:00:36 -0700

    Finished Sprint1 Final...Actual?

--- app/src/main/java/com/example/chordbuilderv2/ChordBuilderDBWrapperUkulele.java
+++ app/src/main/java/com/example/chordbuilderv2/ChordBuilderDBWrapperUkulele.java
@@ -4,19 +4,14 @@ import android.database.Cursor;
 
 public class ChordBuilderDBWrapperUkulele {
     private Cursor cursor;
-
+    String name = "";
     public ChordBuilderDBWrapperUkulele(Cursor cursor){
         this.cursor = cursor;
     }
 
     public String getName(){
-        String name = "";
-        if( cursor != null && cursor.moveToFirst() ){
-            int col = cursor.getColumnIndex("name");
-            name = cursor.getString(col);
-            cursor.close();
-        }
-
+        int col = cursor.getColumnIndex("name");
+        name = cursor.getString(col);
         return name;
     }
 
