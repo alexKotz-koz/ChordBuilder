ed3b958fefbbdf66905e414e1ec3d6375f648bb7
Taynaners <taylor.davis@western.edu>
2021-04-20 08:46:38 -0600

    Metronome finished

--- app/src/main/java/com/example/chordbuilderv2/MetronomeActivity.java
+++ app/src/main/java/com/example/chordbuilderv2/MetronomeActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.res.AssetFileDescriptor;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.media.SoundPool;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -21,45 +23,39 @@ import java.util.ArrayList;
 import java.util.Timer;
 
 public class MetronomeActivity extends AppCompatActivity {
-    MediaPlayer mp;
+    SoundPool soundPool;
     EditText bpm;
     Button startPlayer;
-    String tickFile;
-    MetronomeThread metronomeThread;
-    private Handler mainHandler;
+    int soundIdTick;
+    boolean isPlaying;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_metronome);
-        tickFile = String.valueOf(R.raw.stick);
 
-        mp = MetronomeMediaPlayer.getMediaPlayer();
         bpm = findViewById(R.id.editTextUserInput);
         startPlayer = findViewById(R.id.buttonStartMediaPlayer);
+        soundPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+        soundIdTick = soundPool.load(this, R.raw.tick, 1);
+
+        isPlaying = false;
+    }
 
 
-        startPlayer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String userInput = String.valueOf(bpm.getText());
-                System.out.println("UI: "+userInput);
-                metronomeThread = new MetronomeThread(getApplicationContext(),mainHandler,bpm);
-                metronomeThread.start();
-            }
-        });
+    public void tapMetronome(View view) {
+        bpm = findViewById(R.id.editTextUserInput);
+        float rate = Integer.parseInt(String.valueOf(bpm.getText()));
+        rate /= 60f;
+        rate /= 2;
 
-        mainHandler = new Handler() {
-            @SuppressLint("SetTextI18n")
-            @Override
-            public void handleMessage(Message msg) {
-                if (msg.what == 1) {
-
-                } else if (msg.what == 2) {
-                    metronomeThread.quit();
-                }
-            }
-        };
-
+        if (!isPlaying) {
+            soundPool.play(soundIdTick, 1, 1, 0, -1, rate);
+            isPlaying = !isPlaying;
+        }
+        else {
+            isPlaying = !isPlaying;
+            soundPool.autoPause();
+        }
     }
 }
