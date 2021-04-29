edc3f4449ab8d587902b0f361d7c92fa65a68cfa
Alex Kotz <alexKotz@github.com>
2021-04-29 08:22:52 -0600

    V4: Background changed, MetronomeActivity has nav bar

--- app/src/main/java/com/example/chordbuilderv2/MetronomeActivity.java
+++ app/src/main/java/com/example/chordbuilderv2/MetronomeActivity.java
@@ -1,8 +1,11 @@
 package com.example.chordbuilderv2;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.AssetFileDescriptor;
 import android.media.AudioManager;
@@ -13,6 +16,9 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.os.Message;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -56,5 +62,41 @@ public class MetronomeActivity extends AppCompatActivity {
             isPlaying = !isPlaying;
             soundPool.autoPause();
         }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case (R.id.item_home):
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                return true;
+            case (R.id.item_instruction):
+                AlertDialog.Builder builder = new AlertDialog.Builder(MetronomeActivity.this);
+                builder.setTitle("INSTRUCTIONS");
+                builder.setMessage("Select an Instrument to Build a Chord \n\nSelect Saved Chords to View Your Chords \n\nSelect Chord Finder to Find Chord From Root ");
+
+                builder.setNegativeButton("CLOSE", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+                builder.show();
+                break;
+            case (R.id.item_metro):
+                Intent intent1 = new Intent(getApplicationContext(),MetronomeActivity.class);
+                startActivity(intent1);
+
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
